57354b08efef4c65742cea6c53989de999aa5b56
claude11112 <ctannous@campus.haifa.ac.il>
2025-08-29 17:23:57 +0300

    setting up the schemas file

---
+++ app/db/schemas.py
@@ -0,0 +1,80 @@
+from pydantic import BaseModel,EmailStr
+from datetime import datetime
+from typing import List,Optional
+
+class UserBase(BaseModel):
+    
+    name:str
+    email:EmailStr
+    
+class UserCreate(UserBase):
+    
+    password:str
+    
+class UserOut(UserBase):
+    
+    id:int
+    
+    class Config:
+        orm_mode=True
+        
+
+class MeetingBase(BaseModel):
+    
+    title:str
+    
+class MeetingCreate(MeetingBase):
+    
+    pass
+
+class MeetingOut(MeetingBase):
+    
+    id :int
+    date:datetime
+    user_id :int
+    
+    class Config:
+        orm_mode=True
+        
+
+class TranscriptBase(BaseModel):
+    content:str
+    
+class TranscriptCreate(TranscriptBase):
+    
+    pass
+
+class TranscriptOut(TranscriptBase):
+    
+    id:int
+    created_at:datetime
+    meeting_id:int
+    
+    class Config:
+        orm_mode=True
+        
+        
+class SummaryBase(BaseModel):
+    summary_text:str
+    
+class SummaryCreate(SummaryBase):
+    pass
+
+class SummaryOut(SummaryBase):
+    
+    id:int
+    created_at:datetime
+    transcript_id:int
+    
+    class Config :
+        orm_mode=True
+        
+class TranscriptWithSummaries(TranscriptOut):
+    summaries:List[SummaryOut]=[]
+    
+class MeetingWithTranscript(MeetingOut):
+    transcript :Optional[TranscriptWithSummaries] = None
+
+class UserWithMeetings(UserOut):
+    meetings:List[MeetingWithTranscript]=[]
+    
